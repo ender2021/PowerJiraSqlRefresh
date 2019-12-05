16345006cf06ac45bf65531676d18112a6eae1fc
ender2021 <meadmiracle@gmail.com>
2019-12-05 14:59:41 -0800

    still trying

--- psake.ps1
+++ psake.ps1
@@ -28,7 +28,8 @@ Task Init {
     "Build System Details:"
     Get-Item ENV:BH*
     "`n"
-    Remove-Module PowerShellGet
+    Remove-Module PowerShellGet -Force
+    Uninstall-Module PowerShellGet -Force
     Install-Module PowerShellGet -RequiredVersion "2.1.2" -Force
     Import-Module PowerShellGet -RequiredVersion "2.1.2" -Force
     "Modules:"
