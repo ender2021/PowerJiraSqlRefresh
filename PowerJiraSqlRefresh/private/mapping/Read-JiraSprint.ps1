9063d3c41d6065ad9b45a689f77cc815b0d56352
ender2021 <meadmiracle@gmail.com>
2020-05-21 12:15:59 -0700

    improved parsing of sprint strings, added error handling

--- PowerJiraSqlRefresh/private/mapping/Read-JiraSprint.ps1
+++ PowerJiraSqlRefresh/private/mapping/Read-JiraSprint.ps1
@@ -16,35 +16,47 @@ function Read-JiraSprint {
     }
     
     process {
-        #extract the meaningful bit from the string
-        $var = $Data -match '(?s)\[(.*)\]'
-        $raw = $Matches[0]
+        try {
+            #extract the meaningful bit from the string
+            $var = $Data -match '(?s)\[(.*)\]'
+            $raw = $Matches[0]
 
-        #have to take a weird approach to parsing due to the delimiter also being a valid character in the Goal value
-        #sample value: "id=865,rapidViewId=187,state=ACTIVE,name=GSIS Sprint 3,goal=Complete PowerBI Registration reporting, complete a POC for report embedding, develop the outline of the student profile and 1 tab, and begin addressing data service .Net core conversions,startDate=2019-09-04T18:54:32.477Z,endDate=2019-09-14T00:00:00.000Z,completeDate=<null>,sequence=865"
-        $sections = $raw -split '='
-        $strArr = @()
-        $strArr += $sections | ForEach-Object {
-            if ($_ -eq "[id") {
-                return ''
-            } elseif ($_.ToString().EndsWith(']')) { 
-                return $_.ToString().Substring(0, $_.ToString().Length - 1)
+            #have to take a weird approach to parsing due to the delimiter also being a valid character in the Goal value
+            #sample value: "id=865,rapidViewId=187,state=ACTIVE,name=GSIS Sprint 3,goal=Complete PowerBI Registration reporting, complete a POC for report embedding, develop the outline of the student profile and 1 tab, and begin addressing data service .Net core conversions,startDate=2019-09-04T18:54:32.477Z,endDate=2019-09-14T00:00:00.000Z,completeDate=<null>,sequence=865"
+            $sections = $raw -split '='
+            $resultObj = @{}
+            $currProp = ''
+            $sections | ForEach-Object {
+                if ($_.ToString().StartsWith('[')) {
+                    $currProp = $_.ToString().Substring(1)
+                } elseif ($_.ToString().EndsWith(']')) {
+                    $resultObj.Add($currProp, $_.ToString().Substring(0, $_.ToString().Length - 1))
+                }
+                else {
+                    $commaIndex = $_.ToString().LastIndexOf(',')
+                    $resultObj.Add($currProp, $_.ToString().Substring(0, $commaIndex))
+                    $currProp = $_.ToString().Substring($commaIndex + 1)
+                }
             }
-            else { return $_.ToString().Substring(0, $_.ToString().LastIndexOf(',')) }
+            #load the parsed array into a custom object
+            [pscustomobject]@{
+                Sprint_Id = [int]$resultObj.id
+                Rapid_View_Id = [int]$resultObj.rapidViewId
+                State = $resultObj.state
+                Name = $resultObj.name
+                Goal = $resultObj.goal
+                Start_Date = if($resultObj.startDate -eq '<null>') { $null } else { [datetime](Get-Date $resultObj.startDate) }
+                End_Date = if($resultObj.endDate -eq '<null>') { $null } else { [datetime](Get-Date $resultObj.endDate) }
+                Complete_Date = if ($resultObj.completeDate -eq '<null>') { $null } else { [datetime](Get-Date $resultObj.completeDate) }
+                Sequence = [int]$resultObj.sequence
+                Refresh_Id = $RefreshId
+            }
         }
-        #load the parsed array into a custom object
-        [pscustomobject]@{
-            Sprint_Id = [int]$strArr[1]
-            Rapid_View_Id = [int]$strArr[2]
-            State = $strArr[3]
-            Name = $strArr[4]
-            Goal = $strArr[5]
-            Start_Date = if($strArr[6] -eq '<null>') { $null } else { [datetime](Get-Date $strArr[6]) }
-            End_Date = if($strArr[7] -eq '<null>') { $null } else { [datetime](Get-Date $strArr[7]) }
-            Complete_Date = if ($strArr[8] -eq '<null>') { $null } else { [datetime](Get-Date $strArr[8]) }
-            Sequence = [int]$strArr[9]
-            Refresh_Id = $RefreshId
+        catch {
+            Write-Error "Failed to parse sprint with data string: $Data"
+            throw $_
         }
+        
     }
     
     end {
