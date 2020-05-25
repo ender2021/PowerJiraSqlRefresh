76f49c51cc360f1a665786c513949aadce8e92cc
ender2021 <meadmiracle@gmail.com>
2020-05-25 15:34:21 -0700

    moved sql build files so they aren't deployed with the module

---
+++ Build-SqlScripts.ps1
@@ -0,0 +1,18 @@
+#set script paths
+$rootPath = "$PSScriptRoot\PowerJiraSqlRefresh\public\sql"
+$dbName = "Jira"
+
+$objectsPath = "$rootPath\01-Create-Objects.sql"
+$lookupsPath = "$rootPath\02-Create-Lookups.sql"
+
+#remove the existing versions
+Remove-Item $objectsPath
+Remove-Item $lookupsPath
+
+#create objects
+$sqlComparePath = "C:\Program Files (x86)\Red Gate\SQL Compare 14\SQLCompare.exe"
+& $sqlComparePath /server1:"localhost" /db1:$dbName /empty2 /sf:"$objectsPath"
+
+#create lookup data
+$sqlDataComparePath = "C:\Program Files (x86)\Red Gate\SQL Data Compare 14\SQLDataCompare.exe"
+& $sqlDataComparePath /server1:"localhost" /db1:$dbName /empty2 /sf:"$lookupsPath" /Include:table:tbl_lk_.*
