4568328989076a5853ff790be893cd426be14532
ender2021 <meadmiracle@gmail.com>
2020-05-27 15:44:10 -0700

    fixed?

--- build.ps1
+++ build.ps1
@@ -61,14 +61,14 @@ function Resolve-Module
         }
     }
 }
-$VerbosePreference = "Continue"
+
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 Resolve-Module -Name Psake, PSDeploy, Pester, BuildHelpers, PowerJira, SqlServer -RequiredVersions @{Pester="3.4.0"}
 
-#Set-BuildEnvironment
+Set-BuildEnvironment
 
-#Invoke-psake .\psake.ps1
+Invoke-psake .\psake.ps1
 
 $host.SetShouldExit([int]( -not $psake.build_success ))
